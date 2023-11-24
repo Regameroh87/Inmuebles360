c80cded18f404b1f4e3c462e98aed42f7f704212
Rodrigo Gamero <rodrigogamero@MacBook-Air-de-Rodrigo.local>
2023-11-24 18:00:20 -0300

    console.log cabeceras

--- Server/src/routes/authRouter.js
+++ Server/src/routes/authRouter.js
@@ -30,6 +30,7 @@ authRouter.get("/google/callback", passport.authenticate("auth-google", {
   failureRedirect: "https://inmuebles-360.vercel.app/login"
 }), (req, res) => {
   console.log("Usuario autenticado:", req.user.email);
+  console.log('Cabeceras de respuesta:', res.getHeaders());
   res.redirect("https://inmuebles-360.vercel.app");
 });
 
